0d5247aaff243ef7de97dec9a042966f7c53f19a
sktaha123 <shaikhtaha10102006@gmail.com>
2025-12-31 17:52:48 +0530

    initializing production of BiznorX

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+
+import { useState } from 'react'
+
+
+function App() {
+
+  return (
+    <>
+     <div>
+      <h1>This website is in contruction. Please check back later!</h1>
+
+     </div>
+    </>
+  )
+}
+
+export default App
