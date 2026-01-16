17e185483c3017358ad57461ce237a32c9a200ea
sktaha123 <shaikhtaha10102006@gmail.com>
2026-01-16 20:50:59 +0530

    favicon

--- src/pages/Trust.jsx
+++ src/pages/Trust.jsx
@@ -60,7 +60,7 @@ export  function Trust() {
   variants={cardVariants}
   initial="hidden"
   whileInView="visible"
- viewport={{ once: true, amount: 0.3 }}
+ viewport={{ once: true, amount: 0.5 }}
 
   className="group flex flex-col items-center lg:items-center text-center space-y-4"
 >
